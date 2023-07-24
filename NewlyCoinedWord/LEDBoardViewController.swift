32a4a4f1caf5e805c02b76b2061b70e34ed67c46
LEESANGNAM <fgh0116@naver.com>
2023-07-24 17:55:05 +0900

    LEDBoard UI

---
+++ NewlyCoinedWord/LEDBoardViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LEDBoardViewController.swift
+//  NewlyCoinedWord
+//
+//  Created by 이상남 on 2023/07/24.
+//
+
+import UIKit
+
+class LEDBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+    
+    
+    
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
