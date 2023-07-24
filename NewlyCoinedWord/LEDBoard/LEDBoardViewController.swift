e5decff9d083577e43d294cd2eea2644af859d04
LEESANGNAM <fgh0116@naver.com>
2023-07-24 23:51:12 +0900

    delete UITextInputMod

--- NewlyCoinedWord/LEDBoard/LEDBoardViewController.swift
+++ NewlyCoinedWord/LEDBoard/LEDBoardViewController.swift
@@ -20,41 +20,26 @@ class LEDBoardViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ledTextField.delegate = self
-        languageChage() // 버튼의 타이틀 업데이트
+//        ledTextField.delegate = self
+        setUpLabel()
+        setUpButton(button: resultButton, title: "보내기")
+        setUpButton(button: languageButton, title: "반짝")
+    }
+    
+    func setUpButton(button: UIButton, title: String){
+        button.setTitle(title, for: .normal)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 2
+        
     }
     
     func setUpLabel(){
         resultLabel.text = "글자를 입력해주세요"
         resultLabel.numberOfLines = 1
         resultLabel.adjustsFontSizeToFitWidth = true //글자 크기 자동조정 활성화
+        resultLabel.font = UIFont.boldSystemFont(ofSize: 150)
+        resultLabel.textAlignment = .center
     }
-  
-    
-    // 현재 키보드의 인풋모드를 받아오는 메서드
-    func getTextInputMode() -> String? {
-        guard let textInputMode = UITextInputMode.activeInputModes.first else { return nil }
-        return textInputMode.primaryLanguage
-    }
-    // ???:  입력모드를 받아서 바꾸고 싶었으나(한영전환) textInputMode는 읽기전용이라 변경 불가능 조금더 고민해봐야함
-    func languageChage() {
-        // 입력 모드를 받아서
-        if let inputMode = ledTextField.textInputMode?.primaryLanguage {
-            if inputMode == "ko-KR" { // 한글이면 버튼 텍스트를 한글로 띄우고
-                languageButton.setTitle("한글", for: .normal)
-            } else { // 영어면 버튼 텍스트를 영어로 띄우고
-                languageButton.setTitle("영어", for: .normal)
-            }
-        }
-        
-    }
-    
-    // 텍스트가 변경 될 때 마다 호출되는 메서드
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        languageChage()
-        return true
-    }
-
     func startBlinkingAnimation() {
             UIView.animate(withDuration: 0.5, delay: 0, options: [.autoreverse, .repeat], animations: {
                 self.resultLabel.alpha = 0 // 라벨의 투명도를 0으로 설정하여 반짝이게 만듭니다.
@@ -69,7 +54,6 @@ class LEDBoardViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func BlinkingLable(_ sender: UIButton) {
         isBlinking.toggle()
-        
         if isBlinking{
             startBlinkingAnimation()
         }else {
@@ -79,8 +63,35 @@ class LEDBoardViewController: UIViewController, UITextFieldDelegate {
     }
     
     
+    @IBAction func returnResult(_ sender: UITextField) {
+        resultLabel.text = ledTextField.text
+    }
+    
+    @IBAction func resultButtonTapped(_ sender: UIButton) {
+        resultLabel.text = ledTextField.text
+    }
+    
     @IBAction func backButtonTapped(_ sender: UIButton) {
         dismiss(animated: true)
     }
     
+    
+    // ???:  입력모드를 받아서 바꾸고 싶었으나(한영전환) textInputMode는 읽기전용이라 변경 불가능 조금더 고민해봐야함  생각해보니 여기선 필요 없을듯...
+//    func languageChage() {
+//        // 입력 모드를 받아서
+//        if let inputMode = ledTextField.textInputMode?.primaryLanguage {
+//            if inputMode == "ko-KR" { // 한글이면 버튼 텍스트를 한글로 띄우고
+//                languageButton.setTitle("한글", for: .normal)
+//            } else { // 영어면 버튼 텍스트를 영어로 띄우고
+//                languageButton.setTitle("영어", for: .normal)
+//            }
+//        }
+//
+//    }
+    
+//    // 텍스트가 변경 될 때 마다 호출되는 메서드
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        languageChage()
+//        return true
+//    }
 }
